97d0e53eb955a693ba9e6ac5d9122cee2136ef69
fatcheetah <git@thefatcheetah.com>
2024-01-07 16:29:01 +0000

    feat(git): init

---
+++ ExtensionMethods.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+public static class ExtensionMethods
+{
+    /// <summary>
+    /// Dumps the specified object to the console, displaying its contents.
+    /// </summary>
+    /// <typeparam name="T">The type of the object to dump this is inferred.</typeparam>
+    /// <param name="obj">The object to dump.</param>
+    /// <param name="depth">The depth of indent used in the cli output.</param>
+    /// <returns>The dumped object.</returns>
+    public static T Dump<T>(this T obj, int depth = 0, int MAX_DEPTH = 5)
+    {
+        if (obj == null)
+        {
+            Console.WriteLine(@"null");
+            return obj;
+        }
+
+        if (obj.GetType() == typeof(byte[]))
+        {
+            var bytes = (byte[])(object)obj;
+            var output = new StringBuilder(bytes.Length * 4);
+
+            var arrayPool = ArrayPool<char>.Shared;
+            var asciiLine = arrayPool.Rent(16);
+
+            for (int i = 0; i < bytes.Length; i += 16)
+            {
+                var lineLength = Math.Min(16, bytes.Length - i);
+                for (int byteIndex = 0; byteIndex < 16; byteIndex++)
+                {
+                    if (byteIndex < lineLength)
+                    {
+                        var currentByte = bytes[i + byteIndex];
+                        output.Append($"{currentByte:X2} ");
+                        asciiLine[byteIndex] = currentByte >= 32 && currentByte <= 126
+                            ? (char)currentByte
+                            : '\u00b7';
+                    }
+                    else
+                    {
+                        output.Append(' ', 3);
+                    }
+                }
+
+                output.AppendFormat(" {0}{1}",
+                    new string(asciiLine, 0, lineLength),
+                    i + 16 < bytes.Length ? Environment.NewLine : "");
+            }
+
+            arrayPool.Return(asciiLine);
+            Console.WriteLine(output.ToString());
+
+            return obj;
+        }
+
+        if (obj is string)
+        {
+            Console.WriteLine(obj);
+            return obj;
+        }
+
+        if (obj.GetType().IsPrimitive)
+        {
+            Console.WriteLine(obj);
+            return obj;
+        }
+
+        if (obj.GetType() == typeof(DateTime))
+        {
+            var date = (DateTime)(object)obj;
+            Console.WriteLine(date.ToString("dd/MM/yyyy HH:mm"));
+            return obj;
+        }
+
+        if (depth == MAX_DEPTH)
+        {
+            Console.WriteLine("...");
+            return obj;
+        }
+
+        var properties = obj.GetType().GetProperties();
+        var values = properties.Select(p =>
+        {
+            var value = p.GetIndexParameters().Length == 0
+                ? p.GetValue(obj, null)
+                : p.GetValue(obj, new object[] { 0 });
+
+            return (value, p.PropertyType, p.Name);
+        }).ToArray();
+
+        var longestName = values.Max(v => v.Name.Length);
+
+        foreach (var (value, type, name) in values)
+        {
+            if (value == null) continue;
+
+            var indentation = new StringBuilder().Insert(0, " ", depth * 2);
+
+            if (value is IEnumerable<object> collection)
+            {
+                Console.WriteLine($"{indentation}\u001b[1m{type.Name}::{name}\u001b[0m");
+
+                foreach (var item in collection)
+                {
+                    item.Dump(depth + 1);
+                }
+            }
+            else
+            {
+                Console.Write($"{indentation}\u001b[1m{name.PadRight(longestName)}\u001b[0m: ");
+
+                var isComplexType = !type.IsPrimitive
+                                    && !type.IsEnum
+                                    && type != typeof(string)
+                                    && type != typeof(decimal);
+                if (isComplexType)
+                {
+                    value.Dump(depth + 1);
+                }
+                else
+                {
+                    value.Dump(depth + 1);
+                }
+            }
+        }
+
+        return obj;
+    }
+
+    public static void IterTimer(Action action, int iterations = 100)
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine($"\u001b[1mRunning {iterations} iterations of: {action.Method.Name}\u001b[0m");
+
+        var times = new long[iterations];
+        for (int i = 0; i < iterations; i++)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            action();
+            stopwatch.Stop();
+            times[i] = stopwatch.ElapsedMilliseconds;
+        }
+
+        var maxTime = times.Max();
+        for (int i = 0; i < iterations; i++)
+        {
+            var barLength = (int)(times[i] / (double)maxTime * 50);
+            var bar = new string('#', barLength);
+            sb.AppendLine($"Run {i + 1,3} : {bar}{times[i]}ms");
+        }
+
+        sb.AppendLine($"* Total : {times.Sum()}ms");
+        Console.Write(sb.ToString());
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,177 @@
+using System.Text;
+using System.IO.MemoryMappedFiles;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Collections.Concurrent;
+using System.Buffers;
+using System.Buffers.Text;
+
+/// <summary>
+/// Represents the main entry point of the program.
+/// </summary>
+class Program
+{
+    private const string FILEPATH = "./measurements.txt";
+
+    /// <summary>
+    /// The main method that is executed when the program starts.
+    /// </summary>
+    /// <param name="args">The command-line arguments.</param>
+    public static void Main(string[] args)
+    {
+        var fileStream = new FileStream(
+            path: FILEPATH,
+            mode: FileMode.Open,
+            access: FileAccess.Read,
+            share: FileShare.ReadWrite,
+            bufferSize: 1,
+            options: FileOptions.RandomAccess | FileOptions.Asynchronous
+        );
+
+        var processor = new Processor(
+            fileStream: fileStream,
+            processorCount: Environment.ProcessorCount
+        );
+
+        var chunks = processor.DivideFileIntoChunks().Dump();
+
+        Debug.Assert(fileStream.Length == chunks.Sum(chunk => chunk.Size), "File length does not match sum of chunk lengths.");
+
+        processor.ReadChunk(chunks[0]);
+        // Parallel.ForEach(chunks, chunk => processor.ReadChunk(chunk));
+
+    }
+}
+
+/// <summary>
+/// Represents a processor that reads chunks of a file.
+/// </summary>
+class Processor(FileStream fileStream, int processorCount = 1)
+{
+    private readonly FileStream _fileStream = fileStream;
+    private readonly int _processorCount = processorCount;
+
+    /// <summary>
+    /// Represents a chunk of a file.
+    /// </summary>
+    public class Chunk(long position, long length)
+    {
+        /// <summary>
+        /// Gets the starting position of the chunk in the file.
+        /// </summary>
+        public long Position { get; private set; } = position;
+
+        /// <summary>
+        /// Gets the length of the chunk in bytes.
+        /// </summary>
+        public long Size { get; private set; } = length;
+    }
+
+    /// <summary>
+    /// Divides the file into chunks based on the number of processors.
+    /// </summary>
+    /// <returns>An array of chunks representing different portions of the file.</returns>
+    public Chunk[] DivideFileIntoChunks()
+    {
+        var chunks = new Chunk[_processorCount];
+        var chunkSizeInBytes = _fileStream.Length / _processorCount;
+
+        long chunkStartPosition = 0;
+        for (var chunkIndex = 0; chunkIndex < chunks.Length; chunkIndex++)
+        {
+            if (chunkStartPosition + chunkSizeInBytes <= _fileStream.Length)
+            {
+                var chunkEndPosition = chunkStartPosition + chunkSizeInBytes;
+                _fileStream.Seek(chunkEndPosition, SeekOrigin.Begin);
+
+                var _ = 0; while ((_ = _fileStream.ReadByte()) != '\n' && _ >= 0) ;
+
+                chunkEndPosition = _fileStream.Position;
+                var chunkLengthInBytes = chunkEndPosition - chunkStartPosition;
+
+                chunks[chunkIndex] = new Chunk(chunkStartPosition, chunkLengthInBytes);
+                chunkStartPosition = chunkEndPosition;
+            }
+            else
+            {
+                chunks[chunkIndex] = new Chunk(chunkStartPosition, _fileStream.Length - chunkStartPosition);
+                break;
+            }
+        }
+
+        _fileStream.Seek(0, SeekOrigin.Begin);
+        return chunks;
+    }
+
+    /// <summary>
+    /// Reading files in segments of the system's page size can be beneficial for performance reasons.  The page size is the unit of data that the operating system's memory manager uses to transfer data between the disk and the system's main memory (RAM). When you read a file, the operating system loads it into memory one page at a time.  If you read a file in chunks that align with the system's page size, you can reduce the number of disk operations and make better use of the system's memory cache. This is because each read operation will fill exactly one page of memory, without any wasted space.  Moreover, many file systems and storage devices are optimized for reading data in multiples of the page size. Reading data in smaller chunks can result in additional overhead, as the system needs to perform more read operations and manage more memory pages.  However, it's important to note that the benefits of reading files in page-sized chunks can depend on the specific characteristics of your system and workload. In some cases, other factors may be more important for performance, such as the layout of the data on disk or the pattern of read and write operations.
+    /// </summary>
+    /// <param name="chunk">The chunk to read.</param>
+    public void ReadChunk(Chunk chunk)
+    {
+        var pageSize = GetPageSize();
+        var buffer = ArrayPool<byte>.Shared.Rent(pageSize);
+
+        try
+        {
+            var bytesRead = 0;
+            while (bytesRead < chunk.Size)
+            {
+                var bufferIndex = 0;
+                var lastNewlineIndex = -1;
+
+                // Read a full page at a time
+                var bytesToRead = Math.Min(pageSize, chunk.Size - bytesRead);
+                var bytesActuallyRead = _fileStream.Read(buffer, 0, (int)bytesToRead);
+                bytesRead += bytesActuallyRead;
+
+                // Process each byte in the buffer
+                for (bufferIndex = 0; bufferIndex < bytesActuallyRead; bufferIndex++)
+                {
+                    var byteRead = buffer[bufferIndex];
+
+                    // If the byte is '\n', remember the position
+                    if (byteRead == '\n')
+                    {
+                        lastNewlineIndex = bufferIndex;
+                    }
+                }
+
+                // If the end of the segment doesn't end with a '\n', move the file stream back
+                if (lastNewlineIndex != bufferIndex - 1)
+                {
+                    _fileStream.Position -= (bufferIndex - lastNewlineIndex);
+                    bytesRead -= (bufferIndex - lastNewlineIndex);
+                    bufferIndex = lastNewlineIndex;
+                }
+
+                // Process the buffer here
+                ProcessBuffer(new Span<byte>(buffer, 0, bufferIndex));
+            }
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(buffer);
+        }
+    }
+
+    public void ProcessBuffer(Span<byte> buffer)
+    {
+        // // // buffer to utf8 string
+        // var utf8String = Encoding.UTF8.GetString(buffer);
+        // utf8String.Dump();
+    }
+
+    [DllImport("libc.so.6")]
+    public static extern long sysconf(int name);
+
+    /// <summary>
+    /// Gets the system's page size.
+    /// </summary>
+    /// <returns>The page size in bytes.</returns>
+    public static int GetPageSize()
+    {
+        const int _SC_PAGESIZE = 30;
+        return (int)sysconf(_SC_PAGESIZE);
+    }
+}
